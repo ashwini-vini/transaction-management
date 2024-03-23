bf6ba73ffbc685f31b388133a4f7f615e1dafade
sachintesla <83389705+sachintesla@users.noreply.github.com>
2024-03-23 15:14:56 +0530

    first commit

---
+++ src/CreateDB.java
@@ -0,0 +1,90 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.util.Scanner;
+
+public class CreateDB {
+    public static void main(String[] args) 
+    {
+        String url = "jdbc:mysql://localhost:3306/tap_bank";
+        String un = "root";
+        String pwd = "root";
+        Connection con = null;
+
+        try {
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(url , un  , pwd);
+
+            Scanner scan = new Scanner(System.in);
+
+            // Login Module
+            System.out.println("<---------------Welcome to Tap Bank -------------->");
+            System.out.println("Enter Account Number:");
+            int acc_num = scan.nextInt();
+            System.out.println("Enter Pin:");
+            int pin = scan.nextInt();
+            PreparedStatement pstmt1 =
+            con.prepareStatement("select * from account where "
+            + "acc_num = ? and pin = ? ");
+            pstmt1.setInt(1, acc_num);
+            pstmt1.setInt(2, pin);
+            ResultSet res1 =pstmt1.executeQuery();
+            res1.next();
+            String name = res1.getString(2);
+            int bal = res1.getInt(4);
+            System.out.println("Welcome "+name);
+            System.out.println("Available balance is: "+bal);
+
+            // Transfer Module
+            System.out.println("<---Transfer Details--->");
+            System.out.println("Enter the beneficiary account number:");
+            int bacc_num = scan.nextInt();
+            System.out.println("Enter the transfer amount");
+            int t_amount = scan.nextInt();
+
+            con.setAutoCommit(false);
+            Savepoint s = con.setSavepoint();
+            PreparedStatement pstmt2 = con.prepareStatement("update account set balance = balance - ? where acc_num = ? ");
+            pstmt2.setInt(1, t_amount);
+            pstmt2.setInt(2, acc_num);
+            pstmt2.executeUpdate();
+            System.out.println("<--- Incoming Credit Request --->");
+            System.out.println(name + " account no " +acc_num + "wants to transfer "+ t_amount);
+            System.out.println("Press Y to receive");
+            System.out.println("Press N to reject");
+            String choice = scan.next();
+            if(choice.equals("Y"))
+             {
+            PreparedStatement pstmt3 =con.prepareStatement("update account set balance = balance + ? where acc_num = ? ");
+            pstmt3.setInt(1, t_amount);
+            pstmt3.setInt(2, bacc_num);
+            pstmt3.executeUpdate();
+            PreparedStatement pstmt4 = con.prepareStatement("select * from account where acc_num = ? ");
+            pstmt4.setInt(1, bacc_num);
+            ResultSet res2 =pstmt4.executeQuery();
+            res2.next();
+
+            System.out.println("Updated balance is :"+res2.getInt(4));
+             }
+             else
+             {
+                con.rollback(s);
+                PreparedStatement pstmt5 = con.prepareStatement("select * from account " + " where acc_num = ? ");
+                pstmt5.setInt(1,bacc_num);
+                ResultSet res2 = pstmt5.executeQuery();
+                res2.next();
+                System.out.println("Existing balance is :"+res2.getInt(4));
+
+             }
+             con.commit();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
